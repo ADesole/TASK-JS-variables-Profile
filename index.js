63216d6f5d66dbc2f7dda27087f266bc4561c4fe
Ali Akbar <Ali.A.Akbar98@hotmail.com>
2022-03-17 18:52:02 +0300

    Made the image variable const

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ let fullName = "Ali Abdullah Akbar"
 let yearOfBirth = 1998
 let hobby = "Sleeping"
 let funFact = " Most facts aren't actually fun"
-let image = "https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=612x612&w=0&h=Atw7VdjWG8KgyST8AXXJdmBkzn0lvgqyWod9vTb2XoE="
+const image = "https://media.istockphoto.com/vectors/user-avatar-profile-icon-black-vector-illustration-vector-id1209654046?k=20&m=1209654046&s=612x612&w=0&h=Atw7VdjWG8KgyST8AXXJdmBkzn0lvgqyWod9vTb2XoE="
 
 
 
